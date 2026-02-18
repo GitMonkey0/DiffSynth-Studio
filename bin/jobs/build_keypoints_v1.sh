c32bbf731072edd770db0ff271217bdd4e699940
luhaotian <luhaotian@bytedance.com>
2026-02-18 15:37:16 +0800

    Refactor evaluation and inference scripts for consistency and clarity

---
+++ bin/jobs/build_keypoints_v1.sh
@@ -0,0 +1,3 @@
+python bin/scripts/extract_landmark.py \
+    --input_dir /mnt/bn/aicoding-lq/luhaotian/projects/DiffSynth-Studio/bin/experiments/videos_v1 \
+    --output_dir /mnt/bn/aicoding-lq/luhaotian/projects/DiffSynth-Studio/bin/experiments/keypoints_v1
--- bin/jobs/infer_control_14b_lora_v1.sh
+++ bin/jobs/infer_control_14b_lora_v1.sh
@@ -1,5 +1,5 @@
 export http_proxy=http://sys-proxy-rd-relay.byted.org:8118 https_proxy=http://sys-proxy-rd-relay.byted.org:8118 no_proxy=.byted.org,.bytedance.net,.byteintl.net
 
-torchrun --nproc_per_node=8 /mnt/bn/aicoding-lq/luhaotian/projects/DiffSynth-Studio/extra/infer/control_14b_lora.py \
-        --jsonl /mnt/bn/aicoding-lq/luhaotian/projects/DiffSynth-Studio/extra/data/egodex/metadata/fun_control/baseline/test_metadata.jsonl \
-        --out_dir /mnt/bn/aicoding-lq/luhaotian/projects/DiffSynth-Studio/extra/infer/outputs \
+torchrun --nproc_per_node=8 /mnt/bn/aicoding-lq/luhaotian/projects/DiffSynth-Studio/bin/scripts/infer_control.py \
+        --jsonl /mnt/bn/aicoding-lq/luhaotian/projects/DiffSynth-Studio/bin/data/metadata_v1/test_metadata.jsonl \
+        --out_dir /mnt/bn/aicoding-lq/luhaotian/projects/DiffSynth-Studio/bin/experiments/videos_v1
