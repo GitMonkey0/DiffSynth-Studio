274840513022bad761b6307c7b087ce23ed03dd0
luhaotian <luhaotian@bytedance.com>
2026-02-22 11:01:32 +0800

    some fixs

--- bin/scripts/baseline/infer.sh
+++ bin/scripts/baseline/infer.sh
@@ -1,5 +1,6 @@
 export http_proxy=http://sys-proxy-rd-relay.byted.org:8118 https_proxy=http://sys-proxy-rd-relay.byted.org:8118 no_proxy=.byted.org,.bytedance.net,.byteintl.net
 
 torchrun --nproc_per_node=8 /mnt/bn/aicoding-lq/luhaotian/projects/DiffSynth-Studio/bin/scripts/baseline/infer.py \
-        --jsonl /mnt/bn/aicoding-lq/luhaotian/projects/DiffSynth-Studio/bin/data/processed/metadata/test_metadata.jsonl \
-        --out_dir /mnt/bn/aicoding-lq/luhaotian/projects/DiffSynth-Studio/bin/experiments/baseline/runs/videos
+        --jsonl /mnt/bn/aicoding-lq/luhaotian/projects/DiffSynth-Studio/bin/data/metadata/test_metadata.jsonl \
+        --out_dir /mnt/bn/aicoding-lq/luhaotian/projects/DiffSynth-Studio/bin/experiments/baseline/runs/videos \
+        --lora_path /mnt/bn/aicoding-lq/luhaotian/projects/DiffSynth-Studio/bin/experiments/baseline/ckpt/epoch-0.safetensors
